a7efa7b73ea40e4588b15a547d2ab9d044479258
Maciej Wiącek <maciejwiacek4@gmail.com>
2023-09-08 20:51:48 +0200

    Initial Commit

---
+++ PracticeMultiplication/PracticeMultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeMultiplicationApp.swift
+//  PracticeMultiplication
+//
+//  Created by Maciej Wiącek on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PracticeMultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
